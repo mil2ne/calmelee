d31f3dbc3f640d84ffe742aded0e4f884612e546
celee <show3070@gmail.com>
2024-10-23 20:58:21 +0900

    #16. Onboarding route - 1

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const OnboardingRoute = () => {
+  return (
+    <div className="min-h-screen w-screen flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Welcome to Cal<span className="text-primary">Melee</span>
+          </CardTitle>
+          <CardDescription>
+            We need the following information to set up your profile
+          </CardDescription>
+        </CardHeader>
+        <form>
+          <CardContent className="flex flex-col gap-y-5">
+            <div className="grid gap-y-2">
+              <Label>Full Name</Label>
+              <Input placeholder="Lee Melee" />
+            </div>
+            <div className="grid gap-y-2">
+              <Label>Username</Label>
+              <div className="flex rounded-md">
+                <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-muted bg-muted text-sm text-muted-foreground">
+                  CalMelee.com/
+                </span>
+                <Input
+                  placeholder="example-user-1"
+                  className="rounded-l-none"
+                />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Submit</Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default OnboardingRoute;
